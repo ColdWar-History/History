6006c090fa4f993e7c3bacde3d6b0959c8c40f10
666mxvbee <mitrasher.negativ@gmail.com>
2026-04-16 11:50:42 +0300

    ci(frontend): add github pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/History/" : "/",
   plugins: [react()],
   server: {
     port: 5173,
@@ -11,4 +12,4 @@ export default defineConfig({
       "/health": "http://localhost:7000"
     }
   }
-});
+}));
